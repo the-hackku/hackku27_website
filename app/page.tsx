0964d3fc2e500d27fedf745820991319ba3c3f06
orabbott <orabbott@gmail.com>
2026-06-18 00:27:49 -0500

    main page in the works

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@
 
 export default function HomePage() { 
   return ( 
-    <div
-    > 
-      Welcome to HackKU 2026!
+    <div className="flex flex-col items-center justify-center -mt-23"> 
+      <div
+        className="border-2 px-100 py-80 bg-[#ffffff]"
+        style={{
+          borderRadius: "0 0 4vw 4vw",
+          borderTopLeftRadius: "50% 50px",
+          borderTopRightRadius: "50% 50px",
+        }}
+      >Hello</div>
     </div> 
   ); 
 }
