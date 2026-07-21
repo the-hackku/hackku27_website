3a8bc9b96cf0b85217726947800504e687a9b967
Auriosi <52893462+Auriosi@users.noreply.github.com>
2026-07-21 18:30:58 -0500

    added meta files to site root

---
+++ app/robots.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        "/api/",
+        "/register",
+        "/profile",
+        "/admin/",
+        "/images/doodles/",
+        "/images/schedule/",
+        "/images/sponsors/",
+        "/images/team/",
+        "/images/default.webp",
+        "/images/duck.webp",
+        "/images/mlh-badge.svg"
+      ],
+    },
+    sitemap: 'https://hackku.org/sitemap.xml',
+  }
+}
